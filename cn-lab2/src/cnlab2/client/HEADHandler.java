9c6a4661e86a2ddbec2163bc8be34a3d6c129d0a
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-02-27 21:59:48 +0100

    Implemented HEADHandler.

--- cn-lab2/src/cnlab2/client/HEADHandler.java
+++ cn-lab2/src/cnlab2/client/HEADHandler.java
@@ -1,19 +1,22 @@
 package cnlab2.client;
 
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class HEADHandler extends Handler {
 
 	public HEADHandler(Client client, URI uri) {
 		super(client, uri);
 	}
-
+	
 	@Override
-	public Response handle() {
-		GETHandler g = new GETHandler(getClient(),getUri());
-		Response r = g.handle();
-		Response head = new Response(r.getHeader(),"");
-		return head;
+	public Response handle() throws UnknownHostException, IOException {
+		Socket socket = getClient().getSocketFor(getUri(), 80);
+		
+		sendString(socket, getRequestString("HEAD"));
+		
+		return getResponse(socket);
 	}
 
 }
