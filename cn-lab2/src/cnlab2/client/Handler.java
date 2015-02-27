b28a80e61f341675a3ed23115bad3c46ff80a795
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-02-27 21:09:38 +0100

    more stuff

--- cn-lab2/src/cnlab2/client/Handler.java
+++ cn-lab2/src/cnlab2/client/Handler.java
@@ -5,12 +5,15 @@ import java.net.Socket;
 public abstract class Handler {
 
 	private Client client;
+	private URI uri;
 	
-	public Handler(Client client){
+	
+	public Handler(Client client, URI uri){
 		setClient(client);
+		setUri(uri);
 	}
 	
-	public abstract Response handle(URI uri);
+	public abstract Response handle();
 
 	public Client getClient() {
 		return client;
@@ -18,5 +21,13 @@ public abstract class Handler {
 
 	private void setClient(Client client) {
 		this.client = client;
+	}
+
+	public URI getUri() {
+		return uri;
+	}
+
+	private void setUri(URI uri) {
+		this.uri = uri;
 	}
 }
