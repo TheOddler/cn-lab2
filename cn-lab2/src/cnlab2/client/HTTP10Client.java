624001c8dadc6048e8dded7bd28d3a3fbde6a3a5
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-02-27 22:56:12 +0100

    Missed a file last commit.

--- cn-lab2/src/cnlab2/client/HTTP10Client.java
+++ cn-lab2/src/cnlab2/client/HTTP10Client.java
@@ -10,16 +10,6 @@ public class HTTP10Client extends Client {
 		super();
 	}
 
-	protected boolean canHaveAsCommand(HTTPCommand command) {
-		if (!super.canHaveAsCommand(command)) {
-			return false;
-		} else {
-			return command == HTTPCommand.GET
-				|| command == HTTPCommand.HEAD
-				|| command == HTTPCommand.POST;
-		}
-	}
-
 	@Override
 	public Socket getSocketFor(URI uri) throws UnknownHostException, IOException {
 		return new Socket(uri.getHost(), uri.getPort());
