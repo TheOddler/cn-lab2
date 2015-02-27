51c9ee6798c5770fc4b01400a8c43ccc12469f16
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-02-27 22:54:47 +0100

    Client client further.

--- cn-lab2/src/cnlab2/client/Client.java
+++ cn-lab2/src/cnlab2/client/Client.java
@@ -8,28 +8,6 @@ public abstract class Client {
 
 	public Client() {
 	}
-
-	protected boolean canHaveAsCommand(HTTPCommand command) {
-		return command != null;
-	}
-
-	protected boolean canHaveAsURI(URI uri) {
-		return uri != null;
-	}
-
-	protected boolean canHaveAsPort(int port) {
-		return port >= 0 && port <= 65535;
-	}
-
-	public void handle(HTTPCommand command, URI uri, int port) throws UnknownHostException, IOException, IllegalArgumentException {
-		if (!this.canHaveAsCommand(command))
-			throw new IllegalArgumentException("Invalid command:" + command.toString());
-		if (!this.canHaveAsURI(uri))
-			throw new IllegalArgumentException("Invalid URI:" + uri.toString());
-		if (!this.canHaveAsPort(port))
-			throw new IllegalArgumentException("Invalid port: " + Integer.toString(port));
-		
-	}
 	
 	public abstract Handler getHandlerFor(String command, URI uri);
 	public abstract Socket getSocketFor(URI uri) throws UnknownHostException, IOException;
