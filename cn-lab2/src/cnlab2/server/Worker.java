41de35149f87c411667135adbf83f0b8b8fcd97f
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-03-01 15:40:34 +0100

    some testing

--- cn-lab2/src/cnlab2/server/Worker.java
+++ cn-lab2/src/cnlab2/server/Worker.java
@@ -6,38 +6,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+import cnlab2.common.Request;
+
 public class Worker implements Runnable {
-
-	private Socket socket;
-
-	public Worker(Socket socket) {
-		setSocket(socket);
-	}
-
-	public Socket getSocket() {
-		return socket;
-	}
-
-	private void setSocket(Socket socket) {
-		this.socket = socket;
-	}
-
-	@Override
-	public void run() {
-		try{
-	      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
-	      DataOutputStream outToClient = new DataOutputStream(getSocket().getOutputStream());
-	      
-	      
-
-	      // Read text from the client, make it uppercase and write it back.
-	      String clientSentence = inFromClient.readLine();
-	      System.out.println("Received: " + clientSentence);
-	      String capsSentence = clientSentence.toUpperCase() + '\n';
-	      outToClient.writeBytes(capsSentence);
-		}catch (IOException e){
-			System.out.println("Something went wrong while handling a request.");
-			e.printStackTrace();
-		}
-	}
+    
+    private Socket socket;
+    
+    public Worker(Socket socket) {
+        setSocket(socket);
+    }
+    
+    public Socket getSocket() {
+        return socket;
+    }
+    
+    private void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(getSocket().getOutputStream());
+            
+            Request r = new Request(inFromClient);
+            System.out.println(r);
+            
+        } catch (IOException e) {
+            System.out.println("Something went wrong while handling a request.");
+            e.printStackTrace();
+        }
+    }
 }
