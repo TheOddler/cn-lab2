06cd474dd4ad3d8a13133eea50445241229415aa
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-03-01 16:26:18 +0100

    check this out

--- cn-lab2/src/cnlab2/server/Handler.java
+++ cn-lab2/src/cnlab2/server/Handler.java
@@ -3,6 +3,11 @@ package cnlab2.server;
 import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 import cnlab2.common.Request;
 import cnlab2.common.Response;
@@ -56,11 +61,31 @@ public class Handler implements Runnable {
     }
     
     private void handleGet(Request req) throws IOException {
-        Response resp = new Response(req.getHeader().getVersion(), 200, "OK", "<html> hi </html>");
-        System.out.println("Looking for file: " + ROOT_DIR + req.getHeader().getUri().getResource());
+        String pathStr = req.getHeader().getUri().getResource();
+        if (req.getHeader().getUri().getResource().equals("/")) {
+            pathStr = "index.html";
+        }
+        
+        // Find path of file
+        Path path = Paths.get(ROOT_DIR + File.separator + pathStr);
+        System.out.println("Looking for file: " + path.toString());
+        
+        // Read file to string
+        List<String> strings = Files.readAllLines(path, Charset.defaultCharset());
+        StringBuilder contentBuilder = new StringBuilder();
+        for (String s : strings) {
+            contentBuilder.append(s);
+            contentBuilder.append("\n");
+        }
+        String content = contentBuilder.toString();
+        
+        // Construct a response
+        Response resp = new Response(req.getHeader().getVersion(), 200, "OK", content);
+        
+        // Send the response
         System.out.println("Sending response:\n" + resp.toString());
         getSocket().getOut().writeBytes(resp.toString());
     }
     
-    private static final String ROOT_DIR = System.getProperty("user.dir") + "server-root" + File.pathSeparator;
+    private static final String ROOT_DIR = System.getProperty("user.dir") + File.separator + "server-root";
 }
