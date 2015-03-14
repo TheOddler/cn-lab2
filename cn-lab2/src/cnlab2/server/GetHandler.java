db750dbdaac4ff7f79c021a758d89239ea2e229c
Pablo Bollansée <pablo.bollansee@gmail.com>
2015-03-14 14:39:22 +0100

    Server get handler now supports if-modified-since. Alse properly reports is file was not found.

--- cn-lab2/src/cnlab2/server/GetHandler.java
+++ cn-lab2/src/cnlab2/server/GetHandler.java
@@ -1,9 +1,15 @@
 package cnlab2.server;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import cnlab2.common.Request;
 import cnlab2.common.Response;
@@ -27,28 +33,83 @@ public class GetHandler extends Handler {
         Path path = Paths.get(pathStr);
         System.out.println("Looking for file: " + path.toString());
         
-        // Read file to string
-        // TODO always just read bytes
-        byte[] content;
-        if (Files.exists(path)) {
-            content = Files.readAllBytes(path);
+        Response resp = null;
+        Date localDate = getModifiedDate(path);
+        
+        System.out.println("Local date: " + localDate);
+        
+        if (localDate != null) {
+            Date remoteDate = null;
+            String dateString = getRequest().getHeader().getHeaderField("If-Modified-Since");
+            try {
+                DateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+                df.setTimeZone(TimeZone.getTimeZone("GMT"));
+            
+                remoteDate = df.parse(dateString);
+            } catch (ParseException e) {
+                System.out.println("Invalid date: " + dateString);
+            }
+            
+            if (remoteDate != null && remoteDate.before(localDate)) {
+                resp = getOKResponse(path);
+            }
+            else {
+                resp = getNotChangedResponse();
+            }
         }
         else {
-            content = new byte[0];
+            resp = getNoFileResponse();
         }
-        
-        // Construct a response
-        Response resp = new Response(
-                getRequest().getHeader().getVersion(),
-                200,
-                "OK",
-                typeOfPath(path.toString()),
-                content);
         
         // Send the response
         System.out.println("Sending response:\n" + resp.toString());
         return resp;
         
+    }
+    
+    private Date getModifiedDate(Path path) {
+        File f = path.toFile();
+        if (!f.exists()) {
+            return null;
+        }
+        
+        Long lastModified = f.lastModified();
+        Date lastModifiedDate = new Date(lastModified);
+        return lastModifiedDate;
+    }
+    
+    private Response getNotChangedResponse() {
+        return new Response(
+                getRequest().getHeader().getVersion(),
+                304,
+                "Not Modified",
+                "text/html",
+                new byte[0]);
+    }
+    
+    private Response getOKResponse(Path path) throws IOException {
+        byte[] content = Files.readAllBytes(path);
+        return new Response(
+                getRequest().getHeader().getVersion(),
+                200,
+                "OK",
+                typeOfPath(path.toString()),
+                content);
+    }
+    
+    private Response getNoFileResponse() throws IOException {
+        return new Response(
+                getRequest().getHeader().getVersion(),
+                404,
+                "Not Found",
+                "text/html",
+                get404ResponseContent());
+    }
+    
+    private byte[] get404ResponseContent() throws IOException {
+        Path path = Paths.get("src/cnlab2/server/FourOFour.html");
+        System.out.println(path.toAbsolutePath());
+        return Files.readAllBytes(path);
     }
     
     public String typeOfPath(String path) {
